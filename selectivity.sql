7062490d9294a1f8210a232161e616519dbfd2b1
David Franco <davidwfranco@gmail.com>
2017-06-28 12:03:51 -0300

    Create selectivity.sql

---
+++ selectivity.sql
@@ -0,0 +1,21 @@
+/* Get the quality of an column or group of columns to be used in an index */
+
+column sum         for 9999999999 head 'Total #| Rows'
+column cnt         for 9999999999 head 'Total #| Dist Values'
+column bsel        for 9999999999 head 'Min #| of Rows'
+column avgsel      for 9999999999 head 'Avg #| of Rows'
+column wsel        for 9999999999 head 'Max #| of Rows'
+column bsel_perc   for 9999999.99 head 'Best|Selectivity [%]'
+column avgsel_perc for 9999999.99 head 'Avg|Selectivity [%]'
+column wsel_perc   for 9999999.99 head 'Worst|Selectivity [%]'
+
+select  sum(a) sum,
+    count(a) cnt,
+    min(a) bsel,
+    round(avg(a),1) avgsel,
+    max(a) wsel,
+    round(min(a)/sum(a)*100,2) as bsel_perc,
+    round(avg(a)/sum(a)*100,2) as avgsel_perc,
+    round(max(a)/sum(a)*100,2) as wsel_perc
+from (select count(*) a from &table group by &column)
+;

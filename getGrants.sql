5f155eede661ee554f0ea98a07071fac98dad83c
David FRANCO <David.FRANCO@CRDF.bnpparibas.com>
2017-12-04 08:07:48 -0200

    Adding a simple script to get the grants of a user through the dba_*_privs

---
+++ getGrants.sql
@@ -0,0 +1,14 @@
+-- get all grants of a specific user, could be done with the get_ddl to, but this way is easier to 
+-- adapt from getting the grants of a user to a table, role or etc.
+
+select 'grant '||granted_role||' to '||grantee||';' 
+  from dba_role_privs 
+ where grantee in ('&USER_NAME');
+
+select 'grant '||privilege||' to '||grantee||';' 
+ from dba_sys_privs 
+where grantee in ('&USER_NAME');
+
+select 'grant '||privilege||' on '||owner||'.'||table_name||' to '||grantee||';' 
+  from dba_tab_privs 
+ where grantee in ('&USER_NAME');

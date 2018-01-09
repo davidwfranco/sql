920946439da660fdb75f601c1107ab8d23d2ab88
David FRANCO <David.FRANCO@CRDF.bnpparibas.com>
2018-01-09 15:47:49 -0200

    change the scripts to receive a commando line parameter instead of asking for it

--- getGrants.sql
+++ getGrants.sql
@@ -1,14 +1,18 @@
 -- Get all grants of a specific user, could be done with the get_ddl to, but this way is easier to 
 -- adapt from getting the grants of a user to a table, role or etc.
 
+var v_owner varchar2(30);
+
+exec :v_owner := '&1';
+
 select 'grant '||granted_role||' to '||grantee||';' 
   from dba_role_privs 
- where grantee in ('&&USER_NAME')
+ where grantee in (:v_owner)
 union all
 select 'grant '||privilege||' to '||grantee||';' 
  from dba_sys_privs 
-where grantee in ('&USER_NAME')
+where grantee in (:v_owner)
 union all
 select 'grant '||privilege||' on '||owner||'.'||table_name||' to '||grantee||';' 
   from dba_tab_privs 
- where grantee in ('&USER_NAME');
+ where grantee in (:v_owner);
